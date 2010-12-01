bb030fec9219f87dba379eb07f74803646dcd574
cbattista <battista.christian@gmail.com>
2010-12-01 16:26:59 -0500

    inputData page.php, which will manage the insertion of data from the subject into the database

---
+++ inputData.php
@@ -0,0 +1,36 @@
+<?php
+
+//inputData.php
+
+if(isset($_GET['sid'])) { 
+	$sid = $_GET['sid'];
+	$data = $_GET['data'];	
+	$date = $_GET['date'];
+	$table = $_GET['table'];
+	
+	$data = explode($data, "\n");
+	foreach ($data as $line) {
+		$line = explode($line, "\t");
+		$trial = $line[0];
+		$value = $line[1];
+		$score = $line[2];
+	}
+	
+	$query = sprintf("INSERT INTO % VALUES (%s, %s, %s, %s, %s)", $table, $sid, $trial, $value, $score, $date);
+	
+	$result = mysql_query(query);
+	
+	if ($result) {
+		echo "success";
+	}
+	else {
+		echo "fail";
+	}
+} 
+else {
+	echo "fail";
+}
+
+?>
+
+
