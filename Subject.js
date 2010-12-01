a53f9e69db8197376a9e6aad2c9b45db7e31e1d2
cbattista <battista.christian@gmail.com>
2010-12-01 16:28:19 -0500

    small mods to make the javascript subject class play nice with inputData.php

--- Subject.js
+++ Subject.js
@@ -2,16 +2,18 @@ function Subject(id=666) {
 	//function to handle the collection of subject data
 	this.id = id;
 	this.data = "";
-	this.homebase = "db address";
+	this.table = "satDef_results";
+	this.homebase = "inputData.php";
 	
 	function inputData(trial, value, score){
 		//add a snippet of data to the client side store
-		this.data = this.data + "|" + trial + "," + value + "," + score;
+		this.data = this.data + "\n" + trial + "\t" + value + "\t" + score;
+		this.data = trim(this.data);
 	}
 	
 	function sendData(trial, value, score)  {
 		//send the client side data store to the home base and clear the client store (if successful)
-		senddata = {sid: this.id, data: this.data, time: timestamp};
+		senddata = {table: this.table, sid: this.id, data: this.data, time: timestamp};
 		$.post(this.homebase, senddata, 
 		function(data){
 			if (data == "success") {
