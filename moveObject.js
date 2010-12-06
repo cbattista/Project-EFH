737a072dd5d93ae0427adaef8c2d72b758ba2983
unknown <kcarmic6@uwo.ca>
2010-12-06 16:58:40 -0500

    Added function for incrementing objects position. Added images to Animal Feeder. added comments

---
+++ moveObject.js
@@ -0,0 +1,21 @@
+//Trying to generalize the move object methods
+
+//This function will set the position of the object
+function setPosition(h,id){
+
+	var objPos = h;
+	h = h + "px";
+	$(id).css("top",h);
+	
+	return objPos;
+}
+
+//THis function will actually change the position of the object
+//For some reason it makes the box move reallllyy fast
+function moveObject(h,id){
+	
+	var position = $(id).position();
+	var objPos = position.top + h;
+	$(id).css("top",objPos);
+	
+}
