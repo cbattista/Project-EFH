15f253fa5bc3dae32c7aad02444dcaf9742e8a7a
unknown <kcarmic6@uwo.ca>
2010-12-21 16:15:41 -0500

    Added images to repository. Generalization of newLevel function should be made, added outline of it.

---
+++ newLevel.js
@@ -0,0 +1,11 @@
+function nextLevel(stimFile, stimList) {
+	//stuff for making a new stimList etc...
+	//setting of game variables - eventually should be retrieved from the database
+	trial = 0;
+	level = level + 1;
+	//stimList = [1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
+	//stimFile = ["steak.png", "banana.png"];
+	//start things off
+	stim = stimList[trial];
+	//subject.inputLevelData(level, score, currentTime.getTime());
+	subject.sendLevelData();
