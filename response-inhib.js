077a3fa4c7d0add19f6a1cf9e91d2470072eec94
Fawkes <fawkes@fawkes-Aspire-5570.(none)>
2011-03-29 19:36:50 -0400

    Added images for show, bomb and carepackage will now be exposed when they enter binocs, score is now adjusted accoring to user's decision

--- response-inhib.js
+++ response-inhib.js
@@ -16,7 +16,7 @@ function nextLevel(){
 	for(i = 0; i <= trials/2; i++ ){
 		stimList = stimList.concat(['cp']);} //cp:= care package
 	
-	stimList.sort(randOrd);
+	stimList = stimList.sort(randOrd);
 	
 	//Initiate new trial
 	nextTrial();
@@ -44,26 +44,39 @@ function nextTrial(){
 
 	//Reset Positioin of the box
 	$("#mysteryBox").css("top",initTop);
+
+	if (trial < stimList.length) { //Trial lasts until we run out of fruit 
+		
+		//Send trial info to server
+		subject.inputData(trial, "stim", stim);
+	}
+	else {
+		//alert('Level Complete');
+		subject.sendData();
+		nextLevel();
+	}
+
 }
 function setDifficulty(){}
 
 function setCue(){}
+
 //Create an array of animations 
 function theBox(id){
 
 	var someBox = new Array;
 	
 	someBox["idle"] = new $.gameQuery.Animation({
-		imageURL: "images/SatDef/"+id+"_box.png"});
+		imageURL: "images/SatDef/mystery_box.png"});
 
 	someBox["explode"] = new $.gameQuery.Animation({
 		imageURL: "images/SatDef/"+id+"_explosion.png"});
 	
 	someBox["exposed"] = new $.gameQuery.Animation({
-		imagesURL: "images/SatDef/"+id+"_exposed.png"});
+		imageURL: "images/SatDef/"+id+"_exposed.png"});
 	
 	someBox["grounded"] = new $.gameQuery.Animation({
-		imagesURL: "images/SatDef/"+id+"_grounded.png"});
+		imageURL: "images/SatDef/"+id+"_grounded.png"});
 
 	return someBox;
 }
@@ -81,7 +94,11 @@ $(function(){
 
 	$.playground().addGroup("background", {height:PLAYGROUND_HEIGHT,width: PLAYGROUND_WIDTH})
 		
-			.addSprite("background",{animation: new $.gameQuery.Animation({imageURL:"images/SatDef/satDefBG.png"})}).end()
+			.addSprite("background",{animation: new $.gameQuery.Animation({imageURL:"images/SatDef/satDefBG.png"}),
+				posx:200,
+				posy:200,
+				width:450,
+				height:565}).end()
 
 		      .addGroup("scene", {height:PLAYGROUND_HEIGHT,width: PLAYGROUND_WIDTH})
 
@@ -110,7 +127,6 @@ $(function(){
 			//	posy:0,
 			//	width:90,
 			//	height:60})
-
 
 	//Give the loading bar functionality
 
@@ -146,21 +162,48 @@ $.playground().registerCallback(function(){
 		hitIt = 1;
 		$("#mysteryBox").setAnimation(box["exposed"]);
 	}
+
 	if(blast == 0){ //If nothing was hit
 	
 		//Phase 3(optional): move box to the ground
 		if(newTop >= hideTop && newTop < groundPos){
 			hitIt = 0;
 			$("#mysteryBox").setAnimation(box["idle"]);
-		}
-	
+			
+			//Evaluate User's decision
+			if(stim == "cp"){
+				
+					//Adjust game score
+						score = 500;
+						totalScore += score;
+
+						//Append to HTML
+						$("#totalScore").html(totalScore);
+						$("#score").html(score);
+
+					//alert("Good Work!");		
+				}
+
+			if(stim == "b"){
+					
+					//Adjust game score
+					score =  -500;
+					totalScore += score;
+
+						//Append to HTML
+						$("#totalScore").html(totalScore);
+						$("#score").html(score);
+					
+					//alert("YOU ARE A HORRIBLE PERSON");
+				}
+			}
+		
 		//Phase 4(optional): what happens when the box hits the ground
 		if(newTop == groundPos){
 			$("#mysteryBox").setAnimation(box["grounded"]);
 			nextTrial();
 	 	}
 	}
-
 },REFRESH_RATE);
 
 	//This is where the keybinding occurs
@@ -174,8 +217,7 @@ $.playground().registerCallback(function(){
 
 			if(hitIt == 1){
 				
-				
-				//Calculate time of button pres and gather/send reaction time data
+				//Calculate time of button press and gather/send reaction time data
 				var d = new Date();
 				t2 = d.getTime();
 				RT = t2 - t1;
@@ -185,8 +227,44 @@ $.playground().registerCallback(function(){
 				$("#mysteryBox").setAnimation(box["explode"]);
 				//alert("You got it!");
 				
+				//Evaluate users decision
+				if(stim == "b"){
+				
+					//Adjust game score
+						score = (1 - (RT/100))*-10;
+						totalScore += score;
+
+						//Append to HTML
+						$("#totalScore").html(totalScore);
+						$("#score").html(score);
+
+					alert("Good Work!");		
+
+				}
+
+				if(stim == "cp"){
+					
+					//Adjust game score
+					score =  -100;
+					totalScore += score;
+
+						//Append to HTML
+						$("#totalScore").html(totalScore);
+						$("#score").html(score);
+					
+					alert("YOU ARE A HORRIBLE PERSON");
+
+				}
+
+				//Send trial data to server
+				
+				//subject.inputData(trial,"score",score);
+				//subject.inputData(trial,"RT",RT);
+
 				//set state variable so next trial can start when box is destroyed
 				blast = 1;
+
+				//Initiate next trial
 				nextTrial();
 			}
 
--- riConstants.js
+++ riConstants.js
@@ -13,11 +13,15 @@ var initTop = 0;
 
 //Intitalize the box array to hold the animatons
 var box = new Array();
+
 //Game variables
 var level = 0;
 var trials = 30;
 var trial = 0;
 var vSpeed = 5;
+var totalScore = 0;
+var score = 0;
+
 
 //State variables
 var blast = 0;//1:= something was hit 0:= nothing was hit
@@ -25,3 +29,20 @@ var hitIt = 0;//1:= object is in range 0:= object is out of range
 
 var subject = new Subject(666,"response-inhib");
 
+//Get browser window specs to se the position of the playground
+function getPlayY(){
+	var BROWSER_WIDTH = $(window).width();
+	var playY = BROWSER_WIDTH/2;
+
+	return playY;	
+}
+
+function getPlayX(){
+	var BROWSER_HEIGHT = $(window).height();
+	var playX = BROWSER_HEIGHT/2;
+
+	return playX;
+}
+
+var playPosX = getPlayX();
+var playPosY = getPlayY();
