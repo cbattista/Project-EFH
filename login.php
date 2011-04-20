60b854a4b5a3396f84029490e31cbb00248274fc
Fawkes <Fawkes@lavabit.com>
2011-04-20 18:26:23 -0400

    changes to login.php, links now work in user.html.

--- login.php
+++ login.php
@@ -1,26 +1,32 @@
+<?php
 $username = $_GET['username'];
 $password = $_GET['password'];
 
-$uname = "OH";
-$pw = "HELL";
-$database="NO";
+$uname = "tbattist_execute";
+$pw = "frontalcortex";
+$database="tbattist_EFH";
 
-mysql_connect(localhost,$uname,$pw);
+mysql_connect(localhost, $uname, $pw);
+
+if (!$con)
+	{
+	die('Could not connect: Hello' . mysql_error());
+	}
+
 mysql_select_db($database);
 
-$query = sprintf("SELECT password FROM username WHERE username = '%s' AND password = '%s'", $username,$password);
+$result = mysql_query("SELECT name FROM users");
 
-$result = mysql_query($query);
+while ($row = mysql_fetch_assoc($result)) {
+	$name = $row['name'];
+	}
+echo $name;
+
+$result = mysql_query("SELECT password FROM users");
 
 while ($row = mysql_fetch_assoc($result)){
-	$userID = $row['username'];
-	$userPw = $row['password'];
+	$pswrd = $row['password'];
 }
-echo $result;
-if($userPw != $username ||$userID != $username  || $result == null){
-	alert("Invalid username/password, Try Again");
-} 
-else {
-	alert("Welcome!");
-	
-}
+echo $pswrd;
+
+?>
