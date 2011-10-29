1bf31ffba90c7e1df8f5b6f3827deaba71ec19ce
Fawkes <kcarmic6@uwo.ca>
2011-10-29 10:30:04 -0400

    Health bars will now reset after coming out of a gameOver in both games

--- games/gonogo.js
+++ games/gonogo.js
@@ -68,8 +68,8 @@ function nextTrial(){
 
 		gameOver();
 
-		setHealth("#city",maxHealth);
-		incHealth("#city");
+		//Reset Health of the city. dec and inc seems to do the trick.Settimeout is so the health is resetted after the screen goes black. Looks more authentic.
+		setTimeout("setHealth(\"#city\",maxHealth);decHealth(\"#city\");incHealth(\"#city\");",1000);
 	
 	}
 	//Reset State variables
@@ -131,8 +131,7 @@ function setDifficulty(userScore){
 		success : function(data) { 
 			diffs = data.split(',');
 			difficulty.trials = parseInt(diffs[0]);
-			difficulty.dropSpeed += parseInt(diffs[1]);
-			difficulty.binocSpeed = difficulty.dropSpeed/3;
+			difficulty.binocSpeed = parseInt(diffs[1])/1.5;
 			difficulty.nogoes = parseInt(diffs[2]) / 100;
 
 		},
@@ -360,7 +359,7 @@ $(function(){
 		}
 
 		if(dropIt == 1){
-			dropSpeed = difficulty.dropSpeed;
+			dropSpeed = defaultSpeed;
 			
 			$("#mysteryBox").css("top", boxPos);
 
