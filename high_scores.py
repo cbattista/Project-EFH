42ade26eddb5508fc1df2957ba080266baddfb5c
christian <battista.christian@gmail.com>
2012-01-04 20:56:44 -0500

    page to create a high scores table

---
+++ high_scores.py
@@ -0,0 +1,27 @@
+from databeast import *
+import cherrypy
+from mako_defs import gp_template
+
+class ScoresApp:
+	"""
+	Display the high scores for each game 
+	1 table per game
+	"""
+	@cherrypy.expose
+	def scores(self):
+		#get a data connection
+		beast = dataBeast("funkyTrain", "level")
+		#get the game ids		
+		gids = beast.distinct("game")
+
+		#collect game names and the top 5 sids/top scores for each game
+		tables = ""
+		for gid in gids:
+			name = beast.execute("SELECT name FROM games where gid = %s" % gid)
+			scores = beast.select("(SELECT name from users where uid = user), score", {'game':gid}, sort="score DESC", limit=5)
+			tables += gp_template.get_def("table").render(title=name, data = scores)
+		
+		return tables
+
+#print CheckCompleted.
+cherrypy.quickstart(ScoresApp())
