3f66c05f744f2f28566f284863236b330558cd08
christian <battista.christian@gmail.com>
2012-01-05 15:04:28 -0500

    some constants

---
+++ constants.py
@@ -0,0 +1,7 @@
+#constants.py
+
+#some general purpose constants used in generating page contents
+scripts = {}
+scripts['jquery'] = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
+scripts['gamequery'] = 'games/source_code/jquery.gamequery-0.5.0.js'
+
